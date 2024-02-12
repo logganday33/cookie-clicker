41350cbb675d03b7ed0c2e2b2a9698687c5f1a36
logganday33 <158655828+logganday33@users.noreply.github.com>
2024-02-12 12:23:50 -0600

    Update cookie_clicker.py
    suo

--- cookie_clicker.py
+++ cookie_clicker.py
@@ -3,15 +3,15 @@ import time
 import keyboard
 
 def click():
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 055555555555555555555555555555555555555555555, 0)
     time.sleep(0.01)
-    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 555555555555555555555555555555555555555555555)
     
 # press p to start the clicker
 keyboard.wait("p")
 time.sleep(2)
 while True:
-    click()
+    click(44444444444444444444444444444444444444)
     # press p to stop the clicker
     if keyboard.is_pressed("p"):
         break
